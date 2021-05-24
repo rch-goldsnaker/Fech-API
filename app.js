659fe7f80596fc491fbbfdf4177ef01146fad9e4
rch-goldsnaker <rch.goldsnaker@gmail.com>
2021-05-24 13:05:20 -0300

    lee datos de una API publica

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var contenido = document.querySelector('#contenido')
 
 function traer(){
-    fetch('texto.txt')
-    .then(data => data.text())
+    fetch('https://randomuser.me/api/')
+    .then(res=>res.json())
     .then(data => {
-        console.log(data)
-        contenido.innerHTML = `${data}`
+        console.log(data.results['0'].email)
+        contenido.innerHTML = `
+        <img src="${data.results['0'].picture.large}" width="100px" class="img-fluid rounded-circle" >
+            <p>Nombre:${data.results['0'].name.last}</p>
+        `
     })
+        
 }
